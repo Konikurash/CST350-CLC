a575bdfb40f516167ec2e1eea5e0419457d3055a
azoller11 <azoller11@gmail.com>
2021-08-28 17:06:45 -0700

    Add project files.

---
+++ CLC Proj/Controllers/GameController.cs
@@ -0,0 +1,24 @@
+﻿using CLC_Proj.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CLC_Proj.Controllers
+{
+    public class GameController : Controller
+    {
+        Board gameBoard;
+        public GameController()
+        {
+            gameBoard = new Board(1, 8);
+        }
+
+        public IActionResult Index()
+        {
+            gameBoard.Grid[0, 0] = null;
+            return View(gameBoard);
+        }
+    }
+}
---
+++ CLC Proj/Controllers/RegisterController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using CLC_Proj.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CLC_Proj.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RegisterFormResults(UserModel user)
+        {
+            if (SubmitRegistration(user) == true)
+            {
+                return View("RegisterFormResults", user);
+            }
+            else
+            {
+                return View("RegisterFormResults", user);
+            }
+        }
+        [HttpPost]
+        public bool SubmitRegistration(UserModel user)
+        {
+            bool success = false;
+
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Users;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            string sqlStatement = string.Format("INSERT INTO dbo.RegInfo (FIRSTNAME, LASTNAME, SEX, AGE, STATE, EMAIL, USERNAME, PASSWORD) VALUES ('{0}', '{1}', '{2}', {3}, '{4}', '{5}', '{6}', '{7}')", user.firstName.ToString(), user.lastName.ToString(), user.sex.ToString(), user.age, user.state.ToString(), user.email.ToString(), user.userName.ToString(), user.password.ToString());
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(sqlStatement, connection);
+                success = true;
+            }
+
+            return success;
+        }
+    }
+}
